0ff19b6e367fbc4754dd418fd535d42829f00f4f
Chatchai <chatchai4566@gmail.com>
2019-05-15 21:46:00 +0700

    Created - Project Criminal Intent

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.java
@@ -0,0 +1,56 @@
+package com.example.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    //region [private members]
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    //endregion
+
+    //region [constructor]
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    //endregion
+
+    //region [getter, setter]
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    //endregion
+}
